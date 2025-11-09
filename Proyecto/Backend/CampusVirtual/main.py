a66367d5d5f4f2d0d65e2b18d69efd762064de33
DaviidValerio <164272592+DaviidValerio@users.noreply.github.com>
2025-11-08 22:35:52 -0600

    Update main.py

--- Proyecto/Backend/CampusVirtual/main.py
+++ Proyecto/Backend/CampusVirtual/main.py
@@ -1,12 +1,16 @@
+# main.py
 from fastapi import FastAPI
 from Modules.Equipo1.Routes.equipo1_router import router as equipo1_router
-
+from models.database_equipo4 import create_db_and_tables_equipo4
 
 app = FastAPI()
 
 app.include_router(
     equipo1_router,
-    prefix="/api/equipo1", 
-    tags=["Equipo 1 - Items"], 
+    prefix="/api/equipo1",
+    tags=["Equipo 1 - Items"],
 )
 
+@app.on_event("startup")
+def startup_event():
+    create_db_and_tables_equipo4()
